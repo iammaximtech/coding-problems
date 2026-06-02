dcd11d5ae9f33e729dab923a4861ffc8c9d77e99
iammaximtech <mahathir.maxim@gmail.com>
2026-06-02 03:02:00 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        seen = {}
+        for char in s:
+            seen[char] = seen.get(char,0)+1
+        for char in t:
+            seen[char] = seen.get(char,0)-1
+            if seen[char] == 0:
+                seen.pop(char)
+        return not seen
+
+        
