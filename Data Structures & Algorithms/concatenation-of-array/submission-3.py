d2e1fae4839b048f8d8d3bdefbc2016c8749287f
iammaximtech <mahathir.maxim@gmail.com>
2026-06-14 19:19:14 -0700

    Add: concatenation-of-array - submission-3

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = [0]* 2*len(nums)
+        left =0
+        right = len(nums)
+        while right < 2*len(nums):
+            ans[left]= nums[left]
+            ans[right]= nums[left]
+            left+=1
+            right+=1
+        return ans
