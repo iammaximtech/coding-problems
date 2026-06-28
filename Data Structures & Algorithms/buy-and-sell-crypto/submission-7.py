6be4604e6a57c89c26ee65496df8cbe7e01a51f4
iammaximtech <mahathir.maxim@gmail.com>
2026-06-27 21:29:59 -0700

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) == 0:
+            return 0
+        currMin = prices[0]
+        ans = 0
+        for i in range(len(prices)):
+            if prices[i] < currMin:
+                currMin = prices[i]    
+            ans = max(ans, prices[i]-currMin)
+        return ans
+
