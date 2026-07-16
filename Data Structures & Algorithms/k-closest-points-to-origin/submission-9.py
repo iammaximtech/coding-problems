ae52b16643d8a8084bb02badf94fbd337f9b7012
iammaximtech <mahathir.maxim@gmail.com>
2026-07-16 14:59:02 -0700

    Add: k-closest-points-to-origin - submission-9

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-9.py
@@ -0,0 +1,24 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        if not points or not k: return []
+        distance = lambda x: x[0]**2 + x[1]**2
+
+        def quickSelect(l,r):
+            pivot = r
+            left = l
+            pivotDistance = distance(points[pivot])
+            for i in range(l,r):
+                if  distance(points[i])< pivotDistance:
+                    points[left], points[i] = points[i], points[left]
+                    left += 1
+            points[left], points[r] = points[r], points[left]
+            return left
+        l, r = 0, len(points)-1
+        pivot = len(points)
+        while pivot!=k:
+            pivot = quickSelect(l,r)
+            if pivot < k:
+                l = pivot +1
+            else:
+                r = pivot -1
+        return points[:k]
