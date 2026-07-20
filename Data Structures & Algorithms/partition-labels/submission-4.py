f1ff474c8fc93e4b5290822998fccdb49304fbf8
iammaximtech <mahathir.maxim@gmail.com>
2026-07-19 18:41:50 -0700

    Add: partition-labels - submission-4

---
+++ Data Structures & Algorithms/partition-labels/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        count = {}
+        ans = []
+        for i in range(len(s)):
+            count[s[i]] = i
+        left = right = 0
+        projectedEnd = 0
+        while right<len(s):
+            projectedEnd = max(projectedEnd, count[s[right]])
+            if projectedEnd == right:
+                ans.append(right-left+1)
+                left = right+1
+            right+=1
+        return ans
