2821a7ca980e0b9a8b7503aea31ea2ccbf233712
iammaximtech <mahathir.maxim@gmail.com>
2026-07-25 07:29:44 -0700

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges)!=n-1:
+            return False
+        nodes = {i:[] for i in range(n)}
+        for e in edges:
+            nodes[e[0]].append(e[1])
+            nodes[e[1]].append(e[0])
+        visited = set()
+        def bfs(i):
+            if i<0 or i==n or i in visited:
+                return 
+            dq =collections.deque()
+            dq.append(i)
+            visited.add(i)
+            while dq:
+                curr = dq.popleft()
+                for c in nodes[curr]:
+                    if c not in visited:
+                        dq.append(c)
+                        visited.add(c)
+        bfs(0)
+        return len(visited)==n
