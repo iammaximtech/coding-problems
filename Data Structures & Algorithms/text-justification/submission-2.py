4b847a623ba191793f8c5f56afa6e332bb9ea15e
iammaximtech <mahathir.maxim@gmail.com>
2026-08-02 22:24:44 -0700

    Add: text-justification - submission-2

---
+++ Data Structures & Algorithms/text-justification/submission-2.py
@@ -0,0 +1,37 @@
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        ans = []
+        currLine = []
+        currLen = 0
+
+        for i in range(len(words)):
+            # curr line already longer than maxwidth
+            if currLen + len(currLine)+ len(words[i]) > maxWidth:
+                spaces = (maxWidth - currLen)//max(1,len(currLine)-1)
+                extra = (maxWidth - currLen)%max(1,len(currLine)-1)
+
+                for j in range(max(1,len(currLine)-1)):
+                    currLine[j]+=" "*spaces
+                    if extra:
+                        currLine[j] += " "
+                        extra -= 1
+                ans.append("".join(currLine))
+                currLine = []
+                currLen = 0
+
+            # curr line can add the word
+            currLine.append(words[i])
+            currLen += len(words[i])
+
+            if i==len(words)-1:
+                for j in range(len(currLine)-1):
+                    currLine[j]+=" "
+                    currLen+=1
+                extra = maxWidth - currLen
+                currLine.append(" "*extra)
+                ans.append("".join(currLine))
+        return ans
+        
+
+
+        
